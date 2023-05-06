7e7cbe3ec81ffffb3389067c5779d390f0ef1a51
groundhog02 <96047965+groundhog02@users.noreply.github.com>
2023-05-06 15:46:34 +0000

    a

--- main.py
+++ main.py
@@ -30,61 +30,65 @@ async def on_message(message):
     # 處理輸入文字
     content = message.content.replace(' ', '').lower
 
-    if message.content.startswith('/de'):
-      if message.content == '/de':
+    if message.content.startswith('/dice'):
+      if message.content == '/dice':
         await message.channel.send("|?_?|")
-  
+      if "help" in message.content:
+        pass
+        
+
+
     # 如果是「!roll」開頭的訊息
-    if message.content.startswith('!roll'):
-        content = content.replace('!roll', '')
+    # if message.content.startswith('!roll'):
+    #     content = content.replace('!roll', '')
 
-        # 骰子數量計算
-        dice_cont = content.split('d')[0]
+    #     # 骰子數量計算
+    #     dice_cont = content.split('d')[0]
 
-        try:
-            dice_cont = int(dice_cont)
+    #     try:
+    #         dice_cont = int(dice_cont)
 
-        except ValueError:
-            error.append('How many dice you roll must be an interger!')
+    #     except ValueError:
+    #         error.append('How many dice you roll must be an interger!')
 
-        # 骰子類型判斷
-        content = content.split('d')[1]
-        dice_type = content.split('>')[0]
-        try:
-            dice_type = int(dice_type)
+    #     # 骰子類型判斷
+    #     content = content.split('d')[1]
+    #     dice_type = content.split('>')[0]
+    #     try:
+    #         dice_type = int(dice_type)
 
-        except ValueError:
-            error.append('Dice type must be an interger!')
+    #     except ValueError:
+    #         error.append('Dice type must be an interger!')
 
-        # 成功判斷
-        if '>' in content:
-            success = content.split('>')[1]
-            try:
-                success = int(success)    
-            except ValueError:
-                error.append('Success condition must be an interger!')
+    #     # 成功判斷
+    #     if '>' in content:
+    #         success = content.split('>')[1]
+    #         try:
+    #             success = int(success)    
+    #         except ValueError:
+    #             error.append('Success condition must be an interger!')
 
-        else:
-            success = 0
+    #     else:
+    #         success = 0
 
-        if len(error) == 0:
-            success_count = 0
-            result_msg = ''
+    #     if len(error) == 0:
+    #         success_count = 0
+    #         result_msg = ''
 
-            # 擲骰子
-            results = [random.randint(1, dice_type) for _ in range(dice_cont)]
+    #         # 擲骰子
+    #         results = [random.randint(1, dice_type) for _ in range(dice_cont)]
 
-            for result in results:
-                if success > 0 and result >= success:
-                    success_count += 1
-                result_msg += f'`{result}`, '
+    #         for result in results:
+    #             if success > 0 and result >= success:
+    #                 success_count += 1
+    #             result_msg += f'`{result}`, '
             
-            await message.channel.send(result_msg)
+    #         await message.channel.send(result_msg)
 
-            if success > 0:
-                await message.channel.send(f'Success: `{success_count}`')
-        else:
-            await message.channel.send(error)
+    #         if success > 0:
+    #             await message.channel.send(f'Success: `{success_count}`')
+    #     else:
+    #         await message.channel.send(error)
 
 # TOKEN 在 Discord Developer 的「BOT」頁取得
-client.run('MTEwNDQyNjc1NTczNDkwOTAxOA.Gogn6h.bp90obl4sXiyvUWUSvWHKVT_vwfs3du39DStWE')
+client.run('MTEwNDQyNjc1NTczNDkwOTAxOA.GYOV56.DCB5JZeKg0nAUlhl4V-nk4iV3s44Hni6ADrDGc')
